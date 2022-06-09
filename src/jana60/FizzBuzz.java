8ad26a1557760ce620b6815dadfe85ddd9e579da
matteocovetti <covetti.matteo@gmail.com>
2022-06-09 13:15:05 +0200

    add ciclo for

--- src/jana60/FizzBuzz.java
+++ src/jana60/FizzBuzz.java
@@ -3,7 +3,14 @@ package jana60;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* stampa numeri da 1 a 100
+		 * per i multipli di 3 stampa Fizz
+		 * per i multipli di 5 stampa Buzz
+		 * per i multipli sia di 3 che di 5 stampa FizzBuzz*/
+		
+		for (int i = 1; i < 101; i++) {
+			System.out.println(i);
+		}
 
 	}
 
