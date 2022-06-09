cbe24509e0f8591dfcc277e70ccba682b41975a8
matteocovetti <covetti.matteo@gmail.com>
2022-06-09 13:23:29 +0200

    add if - esercizio finito

--- src/jana60/FizzBuzz.java
+++ src/jana60/FizzBuzz.java
@@ -8,8 +8,20 @@ public class FizzBuzz {
 		 * per i multipli di 5 stampa Buzz
 		 * per i multipli sia di 3 che di 5 stampa FizzBuzz*/
 		
+		System.out.println("Giochiamo a FizzBuzz:");
 		for (int i = 1; i < 101; i++) {
-			System.out.println(i);
+			// add FizzBuzz
+			if ((i % 3) == 0 && (i % 5) == 0) {
+				System.out.println("FizzBuzz");
+			} // add Fizz
+			else if ((i % 3) == 0) {
+				System.out.println("Fizz");
+			} // add Buzz
+			else if ((i % 5) == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
 		}
 
 	}
